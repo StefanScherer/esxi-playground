903a8fed3db90c6e1bf9f6f195d9a8245d51106a
Stefan Scherer <scherer_stefan@icloud.com>
2015-05-10 17:58:55 +0200

    frankus0512 version of vagrant-esxi plugin

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -5,4 +5,5 @@ choco install -y dotnet3.5
 cmd /C choco install -y vmwarevsphereclient -version 6.0.0 -source c:\vagrant\chocolatey\vmwarevsphereclient.6.0.0.nupkg -force
 
 choco install -y vagrant
-vagrant plugin install c:\vagrant\plugins\vagrant-esxi-0.9.0.gem
+# plugin from github.com/frankus0512/vagrant-esxi branch newfeatures
+vagrant plugin install c:\vagrant\plugins\vagrant-esxi-0.1.1.gem
