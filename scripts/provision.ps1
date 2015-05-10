be7290792b66d2bf3af2a306adfc0bb95b5c2a85
Stefan Scherer <scherer_stefan@icloud.com>
2015-05-10 15:29:46 +0200

    install vagrant and vagrant-esxi plugin

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -2,4 +2,7 @@ iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.p
 
 choco install -y dotnet3.5
 # choco install -y vmwarevsphereclient -version 5.5.0.1993072
-cmd /C choco install -y vmwarevsphereclient -version 6.0.0 -source c:\vagrant\scripts\vmwarevsphereclient.6.0.0.nupkg -force
+cmd /C choco install -y vmwarevsphereclient -version 6.0.0 -source c:\vagrant\chocolatey\vmwarevsphereclient.6.0.0.nupkg -force
+
+choco install -y vagrant
+vagrant plugin install c:\vagrant\plugins\vagrant-esxi-0.9.0.gem
