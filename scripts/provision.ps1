435e72ec0863449e0c2d7bc152a480317387d8f7
Stefan Scherer <scherer_stefan@icloud.com>
2015-05-11 18:55:26 +0200

    improve provisioning, add -dv to debug choco

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -1,14 +1,21 @@
+schtasks /End /TN "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
+schtasks /Change /TN "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /DISABLE
+
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install -y dotnet3.5
+choco install -dv -y dotnet3.5
 # choco install -y vmwarevsphereclient -version 5.5.0.1993072
-cmd /C choco install -y vmwarevsphereclient -version 6.0.0 -source c:\vagrant\chocolatey\vmwarevsphereclient.6.0.0.nupkg -force
+choco install -dv -y vmwarevsphereclient -version 6.0.0 -source c:\vagrant\chocolatey
 
-choco install -y vagrant
+choco install -dv -y vagrant
+
 # plugin from github.com/frankus0512/vagrant-esxi branch newfeatures
+$env:Path = $env:Path + ";C:\HashiCorp\Vagrant\bin"
 vagrant plugin install c:\vagrant\plugins\vagrant-esxi-0.1.1.gem
 
 # Download vagrant SSH key to login to ESXi server
-New-Item -Path "C:\Users\vagrant\.ssh" -ItemType Directory
+If (! (Test-Path "C:\Users\vagrant\.ssh")) {
+  New-Item -Path "C:\Users\vagrant\.ssh" -ItemType Directory
+}
 (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub', 'C:\Users\vagrant\.ssh\id_rsa.pub')
 (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant', 'C:\Users\vagrant\.ssh\id_rsa')
