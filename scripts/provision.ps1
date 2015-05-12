8455f961ba37c05bf11815af9da8d9e42202f234
Stefan Scherer <scherer_stefan@icloud.com>
2015-05-12 11:58:25 +0200

    fix provisioning with new VMware basebox

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -1,13 +1,10 @@
-schtasks /End /TN "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
-schtasks /Change /TN "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /DISABLE
-
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install -dv -y dotnet3.5
+choco install -y dotnet3.5
 # choco install -y vmwarevsphereclient -version 5.5.0.1993072
-choco install -dv -y vmwarevsphereclient -version 6.0.0 -source c:\vagrant\chocolatey
+choco install -y vmwarevsphereclient -version 6.0.0 -source c:\vagrant\chocolatey
 
-choco install -dv -y vagrant
+choco install -y vagrant
 
 # plugin from github.com/frankus0512/vagrant-esxi branch newfeatures
 $env:Path = $env:Path + ";C:\HashiCorp\Vagrant\bin"
